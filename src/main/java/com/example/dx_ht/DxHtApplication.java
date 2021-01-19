06f423a1bd9ea95ebc529c22169dfea6b15aaf82
zww <896867024@qq.com>
2021-01-19 11:09:05 +0800

    搭建

---
+++ src/main/java/com/example/dx_ht/DxHtApplication.java
@@ -0,0 +1,13 @@
+package com.example.dx_ht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DxHtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DxHtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/dx_ht/DxHtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dx_ht;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DxHtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
